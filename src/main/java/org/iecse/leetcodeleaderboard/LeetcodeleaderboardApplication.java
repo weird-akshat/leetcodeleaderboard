2b4494d0ff81460db0f86553941af591dda04a35
Akshat Pandey <thatweirdakshat@gmail.com>
2026-01-21 12:57:44 +0530

    set up project

---
+++ src/main/java/org/iecse/leetcodeleaderboard/LeetcodeleaderboardApplication.java
@@ -0,0 +1,16 @@
+package org.iecse.leetcodeleaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeleaderboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeleaderboardApplication.class, args);
+	}
+
+}
+
+//the purpose of this project is to build a leetcode ranking application for my juniors to promote leetcode culture inside the club.
+//going to be a smal
---
+++ src/test/java/org/iecse/leetcodeleaderboard/LeetcodeleaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iecse.leetcodeleaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeleaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
